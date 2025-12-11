fd71cd9946c15c1bace302c10eebce43241d32da
Harrison <80791359+hgreen777@users.noreply.github.com>
2025-12-11 16:57:10 +0000

    Day 2 Part 2

--- src/day2/main.cpp
+++ src/day2/main.cpp
@@ -1,16 +1,15 @@
-
+// Day 2:
 #include "FileHandler.hpp"
 #include <vector>
 #include <string>
 #include <iostream>
-// less then 31000881098
 
 int main()
 {
-    std::vector<std::string> f = FileReader::readCSV("src/day2/input.txt");
+    std::vector<std::string> f = FileReader::readCSV("src/day2/tinput.txt");
 
     long long total = 0;
-    std::string first,last, current;
+    std::string first,last, current, sub, remaining, checkSection;
     long long ifirst,ilast, middle;
     int dashPos;
 
@@ -26,16 +25,43 @@ int main()
 
         for (long long i = ifirst; i <= ilast; i++)
         {
-            // Check if first half of the string == second half.
             current = std::to_string(i);
             middle = current.size() / 2;
 
-            if (current.substr(0, middle) == current.substr(middle))
+            bool match = false;
+
+            for (int subSize = 1; subSize < middle + 1; subSize++)
             {
-                std::cout << current << ", ";
-                total += i;
+                // State 1, Create the possible substri (due to spec will alwasys be the first part)
+                sub = current.substr(0, subSize);
+
+                if (current.size() % subSize == 0)
+                {
+                    // Build a string containing the substring repeated for full length of string.
+                    std::string repeated = "";
+
+                    for (int j = 0; j < current.size() / subSize; j++)
+                    {
+                        repeated += sub;
+                    }
+
+                    //std::cout << repeated << "\n";
+                    
+                    // Check if match.
+                    if (repeated.compare(current) == 0)
+                    {
+                        match = true;
+                        break;
+                    }
+                }
             }
 
+            if (match)
+            {
+                // Success so add total
+                total += i;
+                std::cout << sub << ": " << current << "\n";
+            }
 
         }
 
