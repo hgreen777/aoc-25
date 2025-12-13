b715aaab980998e4a611bd7fc2b508874013cc65
Harrison <hgreendg7@gmail.com>
2025-12-13 18:04:13 +0000

    Day 3 Part 2

--- src/day3/main.cpp
+++ src/day3/main.cpp
@@ -1,17 +1,22 @@
 // Day 3:
-// Part 1:
-// Part 2: 
+// Part 1: 17142
+// Part 2: 169935154100102
 #include <iostream>
 #include <string>
 #include <vector>
 
 #include "FileHandler.hpp"
 
+/*
+814247773322911 -> 844777332291
+*/
+static int smallestIndex (std::string s);
+
 int main()
 {
-    std::vector<std::string> f = FileReader::readLines("src/day3/tinput.txt");
+    std::vector<std::string> f = FileReader::readLines("src/day3/input.txt");
 
-    int total = 0;
+    unsigned long long total = 0ULL;
 
     std::string line,highest = "0";
     char cCurrent;
@@ -19,56 +24,64 @@ int main()
     // For Each line.
     for (int i = 0; i < f.size(); i++)
     {
-        std::cout << f[i]<< "\n";
+        std::cout << "Initial Number: " << f[i]<< "\n";
         line = f[i];
 
-        highest = "00";
+        //std::cout << line.size() << "\n"; // 15
 
-        for (int j = 0; j < line.size() - 1; j++)
+        highest = line.substr(line.size() - 12);
+        std::cout << "Initial Highest: " << highest << "\n";
+
+        // Assume it is going to go from the edge of the current highest to start
+        for (int j = line.size() - 13; j >= 0 ; j--)
         {
 
             cCurrent = line[j];
             //std::cout << "Current Letter: " << cCurrent << "\n";
             
-            // A change is required as it is higher then 1 or both of current.
-            if (cCurrent > highest[0] || cCurrent > highest[1])
+            if (cCurrent >= highest[0])
             {
+                // Shift all current highest along and omit the smalllest number in highest
+                int iSmallest = smallestIndex(highest);
                 
-                if (cCurrent > highest[0])
-                {
-                    // Case 1: Bigger then first -> will be bigger number.
-
-                    if (j == (line.size() - 2))
-                    {
-                        // Edge case: to ensure final highest is valid
-                        highest[1] = cCurrent;
-                        std::cout << "Last Letter edge case" << "\n";
-                    }
-                    else
-                    {
-                        highest[0] = cCurrent;
-                        highest[1] = '0';
-                    }
-                    
-                    std::cout << "Case 1 or 2: " << highest << "\n";
-                }
-                else 
-                {
-                    // Case 3: Bigger then second.
-                    highest[1] = cCurrent;
-                    std::cout << "Case 3: " << highest << "\n";
-                }
+                // Shift up till that index (ie omitting it)
+                highest = std::string(1, cCurrent) + highest.substr(0, iSmallest) + highest.substr(iSmallest + 1);
             }
-            
+            // Remove impurities within the string
+            //std::cout << "Current Highest: " << highest << ". With current letter: " << cCurrent << "\n";
         }
 
 
         // Add final total joltage
-        std::cout << highest << "\n";
-        total += std::stoi(highest);
+        std::cout << "Final: " << highest << "\n";
+        total += std::stoll(highest);
     }
 
     std::cout << total << "\n";
 
     std::cout << std::endl;
+}
+
+// Adapt to take the first smallest index before it rises -> removes impurities
+static int smallestIndex (std::string s)
+{
+    // Input:: String s
+    // Output: Index of where the smallest character number is 
+    char smallest = s[0];
+    int index = 0;
+
+    for (int i = 1; i < s.size(); i++)
+    {
+        if (s[i] < smallest)
+        {
+            smallest = s[i];
+            index = i;
+        }
+        else if (s[i] > smallest)
+        {
+            return index;
+        }
+    }
+
+    return index;
 }
