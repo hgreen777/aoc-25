9d57587d1c02ca89b95566b7e4e2fe434601d1a7
Harrison <hgreendg7@gmail.com>
2025-12-14 14:06:20 +0000

    Day 4 Part 1

---
+++ src/day4/main.cpp
@@ -0,0 +1,69 @@
+// Day 4
+// Part 1: 1547
+// Part 2:
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "FileHandler.hpp"
+
+int main()
+{
+    std::vector<std::string> f = FileReader::readLines("src/day4/input.txt");
+
+    int total = 0;
+    int currentCount = 0;
+    int checkX,checkY;
+
+
+    for (int y = 0; y < f.size(); y++)
+    {
+        for (int x = 0; x < f[0].size(); x++)
+        {
+            if (f[y][x] == '.')
+            {
+                continue;
+            }
+
+            currentCount = 0;
+
+
+            // For each square apply kernal, check bounds. 
+            for (int kernelY = -1; kernelY < 2; kernelY++)
+            {
+                for (int kernelX = -1; kernelX < 2; kernelX++)
+                {
+                    // Dont include current position
+                    if (kernelX == 0 && kernelY == 0)
+                    {
+                        continue;
+                    }
+
+                    checkX = x + kernelX;
+                    checkY = y + kernelY;
+
+                    // Bounds check
+                    if (checkY >= 0 && checkY < f.size() && checkX >= 0 && checkX < f[0].size())
+                    {
+                        if (f[checkY][checkX] == '@')
+                        {
+                            currentCount++;
+                        }
+
+                    }
+                }
+            }
+
+            if (currentCount < 4)
+            {
+                total++;
+                std::cout << "Current Count: " << currentCount << ". For (" << x << ", " << y << ")" << "\n";
+            }
+        }
+    }
+
+    std::cout << total << "\n";
+
+    std::cout << std::endl;
+}
