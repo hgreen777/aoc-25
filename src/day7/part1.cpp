c70b8be4832a476ead1d2bfa09502ebad8547779
Harrison <hgreendg7@gmail.com>
2025-12-17 23:21:38 +0000

    Day 7 Part 1

---
+++ src/day7/part1.cpp
@@ -0,0 +1,72 @@
+// Day 7
+// Part 1: 1581
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+#include "FileHandler.hpp"
+#include "StrUtility.hpp"
+
+#define ADD 0
+#define MULTI 1
+#define MAX_OPERANDS 10
+
+int main()
+{
+    std::cout << "Hello World";
+    std::vector<std::string> f = FileReader::readLines("src/day7/input.txt");
+
+    std::set<int> currentRow;
+    std::set<int> nextRow;
+    int splitTotal = 0;
+
+    // Find Start
+    for (int col = 0; col < f[0].size(); col++)
+    {
+        if (f[0][col] == 'S')
+        {
+            std::cout << "Starting Position: " << col << "\n";
+            currentRow.emplace(col);
+            break;
+        }
+    }
+
+    for (int i = 0; i < f.size() - 1; i++)
+    {
+        for (int col : currentRow)
+        {
+            char next = f[i + 1][col]; 
+            if (next == '.')
+            {
+                nextRow.emplace(col);
+            }
+            else
+            {
+                splitTotal++;
+                std::cout << "Split at: (" << i+1 << ", " << col << "). Current Total Now: " << splitTotal << "\n";
+                nextRow.emplace(col-1);
+                nextRow.emplace(col+1);
+            }
+        }
+
+        currentRow = std::move(nextRow);
+        nextRow.clear();
+
+
+        // Iterate current row
+            // Check row + 1 same col
+            // if '.' 
+                // next row to include col
+            // else
+                // split
+                // add -1 & +1 to next row
+                // increase counter
+    }
+
+
+        
+    std::cout << "Part 1 Total: " << splitTotal;
+    std::cout << std::endl;
+}
